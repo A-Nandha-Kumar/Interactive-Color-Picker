cd8eb94634cec13f1ce9cf9e8592d073c8127cac
A-Nandha-Kumar <nandhakumar2852005@gmail.com>
2025-01-26 19:07:58 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,26 @@
+let colorPickerContainer = document.getElementById("colorPickerContainer");
+let selectedColorHexCode = document.getElementById("selectedColorHexCode");
+
+function action1() {
+    let bgColor = document.getElementById("button1").textContent;
+    colorPickerContainer.style.backgroundColor = bgColor;
+    selectedColorHexCode.textContent = bgColor;
+}
+
+function action2() {
+    let bgColor = document.getElementById("button2").textContent;
+    colorPickerContainer.style.backgroundColor = bgColor;
+    selectedColorHexCode.textContent = bgColor;
+}
+
+function action3() {
+    let bgColor = document.getElementById("button3").textContent;
+    colorPickerContainer.style.backgroundColor = bgColor;
+    selectedColorHexCode.textContent = bgColor;
+}
+
+function action4() {
+    let bgColor = document.getElementById("button4").textContent;
+    colorPickerContainer.style.backgroundColor = bgColor;
+    selectedColorHexCode.textContent = bgColor;
+}
